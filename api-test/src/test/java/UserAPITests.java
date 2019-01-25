e823a9c3c24d67293e59fa5b4bc518162d339088
katerynaoleksiienko <katerynaoleksiienko@github.com>
2019-01-25 15:07:35 +0200

    last

--- api-test/src/test/java/UserAPITests.java
+++ api-test/src/test/java/UserAPITests.java
@@ -1,4 +1,35 @@
+import com.example.model.UserPayload;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.isEmptyString;
+
 public class UserAPITests {
 
-    ///
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://139.59.130.228";
+    }
+//https://www.tutorialspoint.com/gradle/gradle_installation.htm
+    @Test
+    void testCanRegisterUser() {
+        UserPayload user = new UserPayload()
+                .setUsername("grg")
+                .setPassword("rr111")
+                .setEmail("rr@fff.com");
+
+        RestAssured
+                .given().contentType(ContentType.JSON).log().all()
+                .body(user)
+                .when()
+                .post("register")
+                .then().log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("id", not(isEmptyString()));
+    }
 }
